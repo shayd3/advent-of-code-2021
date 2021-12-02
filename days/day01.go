1cc555a61a0c3724d764b29c3d2c93fc0eb1c9dc
shayd3 <ryan.ross@temple.edu>
2021-12-01 21:31:32 -0500

    Day 1 Part 2, complete!

--- days/day01.go
+++ days/day01.go
@@ -9,11 +9,41 @@ import (
 // Day01 - https://adventofcode.com/2021/day/1
 func Day01() {
 	inputs := inputs.Day01
+	fmt.Println(getNumberOfIncreases(inputs, 1))
+	fmt.Println(getNumberOfIncreases(inputs, 3))
+}
+
+// getNumberOfIncreases calculates how many times 
+// there are positive increases on the previous number
+// in the given input. 'window' is how many of the inputs
+// are included in the comparison sums
+func getNumberOfIncreases(inputs []int, window int) int {
 	count := 0
-	for i := 1; i < len(inputs); i++ {
-		if (inputs[i-1] < inputs[i]) {
-			count++
-		}
+	// Check if window is out of bounds or if no window at all
+	if (len(inputs) < window || window <= 0 ) {
+		return count
 	}
-	fmt.Println(count)
+
+	for i := window; i < len(inputs); i++ {
+		// Only compare sums of windows if window is more that 1
+		if(window > 1) {
+			windowASum, windowBSum := 0, 0
+			for _, valueA := range inputs[i-window : i] {
+				windowASum += valueA
+			}
+			for _, valueB := range inputs[i-window+1 : i+1] {
+				windowBSum += valueB
+			}
+			if(windowASum < windowBSum) {
+				count++
+			}
+
+		} else {
+			if (inputs[i-1] < inputs[i]) {
+				count++
+			}
+		}
+		
+	}
+	return count
 }
