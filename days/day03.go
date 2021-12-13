d88fbec57e0984ea922fb13363f35e5eee5fc6d2
shayd3 <ryan.ross215@gmail.com>
2021-12-13 12:14:32 -0500

    Day 3 pt 2 completed. I got lazy :)

--- days/day03.go
+++ days/day03.go
@@ -19,16 +19,20 @@ type ReportResult struct {
 func Day03() {
 	inputs := inputs.Day03
 	reportResults := generateReportResults(inputs)
-	fmt.Printf("Report Product: %d", getPowerConsumption(reportResults))
-	
+	fmt.Printf("Report Product: %d\n", getPowerConsumption(reportResults))
+	fmt.Printf("Life Support Rating: %d\n", getLifeSupportRating(reportResults))
 }
 
 func generateReportResults(inputs []string) ReportResult {
 	reportResults := ReportResult{}
 	gamma, epsilon := getPowerConsumptionProperties(inputs)
-
+	co2Scrubber := getCO2ScrubberRating(inputs)
+	oxygenGenerator := getOxygenGeneratorRating(inputs)
+	
 	reportResults.gamma = gamma
 	reportResults.epsilon = epsilon
+	reportResults.oxygenGenerator = oxygenGenerator
+	reportResults.co2Scrubber = co2Scrubber
 	return reportResults
 }
 
@@ -36,6 +40,8 @@ func getPowerConsumptionProperties(inputs []string) (gamma int64, epsilon int64)
 	gammaRaw := ""
 	epsilonRaw := ""
 
+	// TODO: Extract the column loops into separate function
+	// TODO: Figure out different way to extract gamma and epsilon values
 	for col := 0; col < len(inputs[0]); col++ {
 		zeros := 0
 		ones := 0
@@ -47,7 +53,7 @@ func getPowerConsumptionProperties(inputs []string) (gamma int64, epsilon int64)
 				ones++
 			}
 		}
-		most, least := getFrequency(ones, zeros)
+		most, least := getFrequency(ones, zeros, 1)
 		gammaRaw += strconv.Itoa(most)
 		epsilonRaw += strconv.Itoa(least)
 	}
@@ -55,8 +61,74 @@ func getPowerConsumptionProperties(inputs []string) (gamma int64, epsilon int64)
 	return convertToBinary(gammaRaw), convertToBinary(epsilonRaw)
 }
 
-func getFrequency(ones int, zeros int) (most int, least int) {
-	if(ones > zeros) {
+func getOxygenGeneratorRating(inputs []string) (oxygenGeneratorRating int64) {
+	foundRating := []string{}
+	// TODO: Extract the column loops into separate function
+	for col := 0; col < len(inputs[0]); col++ {
+		zeros := 0
+		ones := 0
+		for row := 0; row < len(inputs); row++ {
+			bit := inputs[row][col]
+			if (bit == '0') {
+				zeros++
+			} else {
+				ones++
+			}
+		}
+		most, _ := getFrequency(ones, zeros, 1)
+		byteMost := strconv.Itoa(most)
+		tmp := []string{}
+		for index, val := range inputs {
+			if inputs[index][col] == byteMost[0] {
+				tmp = append(tmp, val)
+			}
+		}
+		inputs = tmp
+		if len(inputs) == 1 {
+			foundRating = inputs
+			break
+		}
+	}
+
+	return convertToBinary(foundRating[0])
+}
+
+func getCO2ScrubberRating(inputs []string) (co2ScrubberRating int64) {
+	foundRating := []string{}
+	// TODO: Extract the column loops into separate function
+	for col := 0; col < len(inputs[0]); col++ {
+		zeros := 0
+		ones := 0
+		for row := 0; row < len(inputs); row++ {
+			bit := inputs[row][col]
+			if (bit == '0') {
+				zeros++
+			} else {
+				ones++
+			}
+		}
+		_, least := getFrequency(ones, zeros, 0)
+		byteLeast := strconv.Itoa(least)
+		tmp := []string{}
+		for index, val := range inputs {
+			if inputs[index][col] == byteLeast[0] {
+				tmp = append(tmp, val)
+			}
+		}
+		inputs = tmp
+		if len(inputs) == 1 {
+			foundRating = inputs
+			break
+		}
+	}
+
+	return convertToBinary(foundRating[0])
+}
+
+func getFrequency(ones int, zeros int, tieBreaker int) (most int, least int) {
+	if(ones == zeros) {
+		return tieBreaker, tieBreaker
+	} else if(ones > zeros) {
 		return 1, 0
 	} else {
 		return 0, 1
@@ -73,4 +145,8 @@ func convertToBinary(str string) int64 {
 
 func getPowerConsumption(reportResult ReportResult) int64 {
 	return reportResult.gamma * reportResult.epsilon
+}
+
+func getLifeSupportRating (reportResult ReportResult ) int64 {
+	return reportResult.co2Scrubber * reportResult.oxygenGenerator
 }
