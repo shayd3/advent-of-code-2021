2d1811944af1defc85d87ae41bb0eecd51a575f0
shayd3 <ryan.ross215@gmail.com>
2021-12-17 15:42:03 -0500

    Rework day 6 to more memory optimized solution. day 6 pt 1 reworked and pt 2 finished

--- days/day06.go
+++ days/day06.go
@@ -8,7 +8,6 @@ import (
 
 const NEW_BORN_LANTERNFISH_LIFE_TIMER = 8
 const LANTERNFISH_LIFE_TIMER = 6
-const TOTAL_DAYS = 80
 
 type Lanternfish struct {
 	lifespan int
@@ -16,41 +15,37 @@ type Lanternfish struct {
 }
 
 func Day06() {
-	//inputs := inputs.Day06Sample
+	// inputs := inputs.Day06Sample
 	inputs := inputs.Day06
-	lanternfishes := parseLanternfish(inputs)
-	totalFish := simulateLanternfish(lanternfishes, TOTAL_DAYS)
-	fmt.Printf("Total number of Lanternfishes after %d days: %d\n", TOTAL_DAYS, totalFish)
-}
-
-func parseLanternfish(input []int) []Lanternfish {
-	lanternfishes := []Lanternfish{}
-	for _, fishLifespan := range input {
-		lanternfishes = append(lanternfishes, Lanternfish{lifespan: fishLifespan})
-	}
-	return lanternfishes
+	totalFishPart1 := simulateLanternfish(inputs, 80, 8, 6)
+	totalFishPart2 := simulateLanternfish(inputs, 256, 8, 6)
+	fmt.Printf("Total number of Lanternfishes after %d days: %d\n", 18, totalFishPart1)
+	fmt.Printf("Total number of Lanternfishes after %d days: %d\n", 256, totalFishPart2)
 }
 
 // simulateLanternfish takes in a starting list of fishes and days to simulate
 // returns total number of lanternfish after n number of given days
-func simulateLanternfish(fishes []Lanternfish, days int) int {
-	for i := 0; i < days; i++ {
-		fmt.Printf("Day #%d => Current Total: %d\n", i+1, len(fishes))
-		newFishToSpawn := 0
-		for i := range fishes {
-			if fishes[i].lifespan == 0 {
-				newFishToSpawn++
-				fishes[i].lifespan = LANTERNFISH_LIFE_TIMER
-				fishes[i].lifespanRefresh = true
-			}
-			if fishes[i].lifespanRefresh == false {
-				fishes[i].lifespan--
-			}
-			fishes[i].lifespanRefresh = false
-		}
-		for i := 0; i < newFishToSpawn; i++ {
-			fishes = append(fishes, Lanternfish{ lifespan: NEW_BORN_LANTERNFISH_LIFE_TIMER})
-		}
+func simulateLanternfish(fishes []int, days int, newBornLifespan int, fishLifespanDayRefresh int) int {
+	fishPopulationSimulation := make([]int, newBornLifespan + 1)
+	// Initlize population simulation with fishes input
+	for _, fish := range fishes {
+		fishPopulationSimulation[fish]++
 	}
-	return len(fishes)
+	for day := 0; day < days; day++ {
+		// Hold first value for all 0s
+		tmp := fishPopulationSimulation[0]
+		for i := 1; i < len(fishPopulationSimulation); i++ {
+			fishPopulationSimulation[i-1] = fishPopulationSimulation[i]
+		}
+		fishPopulationSimulation[fishLifespanDayRefresh] += tmp
+		fishPopulationSimulation[newBornLifespan] = tmp
+		//fmt.Printf("Fish population Day#%d => \n\t%v\n", day+1, fishPopulationSimulation)
+	}
+	
+
+	sum := 0
+	for _, val := range fishPopulationSimulation {
+		sum += val
+	}
+	return sum
 }
