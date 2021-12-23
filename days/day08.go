66713d05e86f0fb605a34c4d76f1cca49279e8bd
shayd3 <ryan.ross@temple.edu>
2021-12-22 20:41:48 -0500

    Day08 part 1 complete!

--- days/day08.go
+++ days/day08.go
@@ -15,11 +15,11 @@ type Entry struct {
 }
 
 func Day08() {
-	input := inputs.Day08Sample
+	// input := inputs.Day08Sample
+	input := inputs.Day08
 	entries := parseInput(input)
-	for _, entry := range entries {
-		fmt.Println(entry)
-	}
+	count := findEasyDigitsCount(entries)
+	fmt.Printf("Total times 1, 4, 7, or 8 appeared: %d\n", count)
 }
 
 func parseInput(input []string) []Entry {
@@ -34,3 +34,38 @@ func parseInput(input []string) []Entry {
 
 	return entries
 }
+/*
+break down of lengths:
+len = number
+
+2 => 1
+3 => 7
+4 => 4
+7 => 8
+
+5 => 2,3,5
+	- Contains 7 -> 3
+	- Else [2,5]
+		- intersects "4" in 3 places -> 5
+		- else -> 2
+6 => 0,6,9
+	- Contains 4 -> 9
+	- Else -> [0,6]
+*/
+
+// findEasyDigitsCount finds total count of how many times
+// 1, 4, 7, and 8 in a slice of Entry objects
+func findEasyDigitsCount(entries []Entry) int {
+	count := 0
+	for _, entry := range entries {
+		for _, outputVal := range entry.outputValue {
+			switch len(outputVal) {
+				case 2: count++; // 1
+				case 3: count++; // 7
+				case 4: count++; // 4
+				case 7: count++; // 8
+			}
+		}
+	}
+	return count
+} 
